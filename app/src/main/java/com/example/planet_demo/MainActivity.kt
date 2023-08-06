bef63cf01f14926410590dc5700c2902f9a819d7
tongueEye <109783402+tongueEye@users.noreply.github.com>
2023-08-06 12:20:36 +0900

    *메인화면-탭바 UI 구현* - 탭바에 있는 아이콘 클릭시 각각 다른 화면이 나오도록 구현 - 아이콘 디자인은 임시로 설정(추후 변경 예정)
    + 로고에 사용된 폰트추가(저작권x)

--- app/src/main/java/com/example/planet_demo/MainActivity.kt
+++ app/src/main/java/com/example/planet_demo/MainActivity.kt
@@ -2,10 +2,50 @@ package com.example.planet_demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.example.planet_demo.navigation.AlarmFragment
+import com.example.planet_demo.navigation.DetailViewFragment
+import com.example.planet_demo.navigation.GridFragment
+import com.example.planet_demo.navigation.UserFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_home -> {
+                var detailViewFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, detailViewFragment).commit()
+                return true
+            }
+            R.id.action_search -> {
+                var gridFragment = GridFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, gridFragment).commit()
+                return true
+            }
+            R.id.action_add_photo -> {
+                return true
+            }
+            R.id.action_favorite_alarm -> {
+                var alarmFragment = AlarmFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, alarmFragment).commit()
+                return true
+            }
+            R.id.action_account -> {
+                var userFragment = UserFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, userFragment).commit()
+                return true
+            }
+        }
+        return false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
     }
+
 }
