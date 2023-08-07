54d6e561f0207637eb534054149b1ffea2ad7dbc
tongueEye <109783402+tongueEye@users.noreply.github.com>
2023-08-07 17:18:28 +0900

    *사진 소개 페이지 구현* - 사진 업로드로 firestore database에 저장된 정보(사진 설명, 유저 이메일 아이디, 사진 등)를 가져와 보여주는 화면 및 기능 구현 - UI는 추후 수정 예정

--- app/src/main/java/com/example/planet_demo/MainActivity.kt
+++ app/src/main/java/com/example/planet_demo/MainActivity.kt
@@ -55,6 +55,9 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         setContentView(R.layout.activity_main)
         bottom_navigation.setOnNavigationItemSelectedListener(this)
         ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1)
+
+        //Set default screen
+        bottom_navigation.selectedItemId=R.id.action_home
     }
 
 }
